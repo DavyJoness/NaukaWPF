45d4d589f9158f8494bc828419369361c3840a53
= <=>
2019-03-27 14:33:54 +0100

    Create

---
+++ NaukaWPF/Rysowanie.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace NaukaWPF
+{
+    /// <summary>
+    /// Interaction logic for Rysowanie.xaml
+    /// </summary>
+    public partial class Rysowanie : Window
+    {
+        public Rysowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+
+            SolidColorBrush scb = new SolidColorBrush();
+            scb.Color = Color.FromArgb(100, 25, 50, 0);
+
+            Ellipse el = new Ellipse();
+            el.Width = 200;
+            el.Height = 100;
+            el.Fill = scb;
+
+            drawSpace.Children.Add(el);
+        }
+
+        private void ButtonDrawRectangle_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+
+            SolidColorBrush scb = new SolidColorBrush();
+            scb.Color = Color.FromArgb(100, 25, 50, 0);
+
+            Rectangle rec = new Rectangle();
+            rec.Width = 200;
+            rec.Height = 100;
+            rec.Fill = scb;
+
+            drawSpace.Children.Add(rec);
+        }
+
+        public void Clear()
+        {
+            drawSpace.Children.Clear();
+        }
+    }
+}
